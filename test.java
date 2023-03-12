c12d3f4c92d72ceb821e5ddf78089d38848dd783
ch prashanth <prashanth.chinta057@gmail.com>
2023-03-12 15:17:20 +0530

    updated in code

--- test.java
+++ test.java
@@ -1,4 +1,7 @@
 class test{
 public static void main(String[] args){
 for(int i=65;i<91;i++){
-System.out.println((char)i);}}}
+System.out.println((char)i);
+}
+for(int i=0;i<100;i++)
+    System.out.println(i);}}
